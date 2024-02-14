86cfc02698470e9ad275dc63bb61aaba691e5685
Mirdcafar <rmirdzafar@gmail.com>
2024-02-14 21:00:58 +0400

    task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿double number = 123.789;
+
+int result = ((int)number);
+
+double res = number - result;
+double res2 = res * 1000;
+
+Console.WriteLine((int)+res2);
