1d1e6c2d80481f3eff1accfc1b1a1b81e1481b6f
Mirdcafar <rmirdzafar@gmail.com>
2024-03-31 19:37:59 +0400

    file

---
+++ PhonShop/Iphone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.PhonShop.FileName;
+
+namespace ConsoleApp1.PhonShop;
+
+public class Iphone : Phon
+{
+    private int time { get; set; }
+    public Iphone(string marka, int price, int time) : base(marka, price, time)
+    {
+
+    }
+
+    public override string GetPhoneInfo()
+    {
+        return $"Marka:{Marka} Price: {Price}, Stock: {time}";
+    }
+    public override int ComingTime
+    {
+        get => time;
+        set
+        {
+            int times = DateTime.Now.Day;
+            time = value;
+            if (time == times)
+            {
+                Console.WriteLine($"Marka:{Marka} Stoka geldi !!!");
+                phonEvent?.Invoke(this);
+            }
+        }
+    }
+    public override event PhonEvent phonEvent;
+
+    public override void Buy(int price)
+    {
+
+        if (Price <= price)
+        {
+            Console.WriteLine($"{Marka} telefonu aldiniz");
+            price -= Price;
+            Console.WriteLine($"Qalan balans: {price}");
+        }
+        else
+        {
+            Console.WriteLine("Balansinizda yeterince mebleg yohdur !!!");
+        }
+    }
+
+}
---
+++ PhonShop/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.PhonShop
+{
+    public class Person
+    {
+        public Person(string name , int price )
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public void BuyPhon(Phon phons)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"{Name} feed {phons.Marka}");
+            phons.Buy(Price);
+        }
+    }
+}
